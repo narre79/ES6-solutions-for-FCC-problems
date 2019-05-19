bff5c37a17a32dc4eccb606a6c3070fbed42f62c
normdev <49796937+narre79@users.noreply.github.com>
2019-05-19 14:46:46 -0400

    Add files via upload

---
+++ Use Destructuring Assignment to Pass an Object as a Function's Parameters.js
@@ -0,0 +1,22 @@
+//Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+const half = (function() {
+  "use strict"; // do not change this line
+
+  // change code below this line dont forget to add braces
+  return ({min, max}) => {
+    // use function argument destructuring
+    return (max + min) / 2.0;
+  };
+  // change code above this line
+
+})();
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015

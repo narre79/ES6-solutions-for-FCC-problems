6be0a7ab350b2ad1069e3b694b226b213cf86ad0
normdev <49796937+narre79@users.noreply.github.com>
2019-06-03 21:43:02 -0400

    Create Basic Data Structures.js

---
+++ Basic Data Structures.js
@@ -0,0 +1,10 @@
+//Basic Data Structures: Add Items Using splice()
+function htmlColorNames(arr) {
+  // Below objectToBeSpliced.splice(startingNumber,numberToBeDeleted,'newObjects')
+  arr.splice(0,2,'DarkSalmon','BlanchedAlmond')
+  // change code above this line
+  return arr;
+}
+
+// do not change code below this line
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurqoise', 'FireBrick']));
